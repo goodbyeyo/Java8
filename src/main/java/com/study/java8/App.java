482644348f486fa551619308e6e21b4eb47a1684
HwangSangWook <goodbyeyo@naver.com>
2022-07-08 20:01:08 +0900

    optaional

--- src/main/java/com/study/java8/App.java
+++ src/main/java/com/study/java8/App.java
@@ -1,7 +1,7 @@
 package com.study.java8;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.time.Duration;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
@@ -9,19 +9,99 @@ public class App {
         String[] names = {"wook", "sumin", "temin"};
 
         // Comparator 함수형 인터페이스
-        Arrays.sort(names, new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2) {
-                return 0;
-            }
-        });
+//        Arrays.sort(names, new Comparator<String>(){
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return 0;
+//            }
+//        });
+//
+//        // 임의 객체의 인스턴스 메소드 참조
+//        Arrays.sort(names, String::compareToIgnoreCase);
+//
+//        Foo foo = new DefaultFoo("wook");
+//        foo.printName();
+//        foo.printNameUpperCase();
+//        Foo.printAnything();
 
-        // 임의 객체의 인스턴스 메소드 참조
-        Arrays.sort(names, String::compareToIgnoreCase);
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        Foo foo = new DefaultFoo("wook");
-        foo.printName();
-        foo.printNameUpperCase();
-        Foo.printAnything();
+        OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
+        // Reference Progress 의 기본값이 null 이기 때문에 nullPointException 발생
+        // Duration studyDuration = spring_boot.getProgress().getStudyDuration();
+        // System.out.println("studyDuration = " + studyDuration);
+        // if (spring_boot.getProgress() != null) {
+        //     System.out.println("spring_boot = " + spring_boot.getProgress().getStudyDuration());
+        // }
+
+        /*
+        Optional<OnlineClass> optional = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .findFirst();
+        */
+
+        /* 값이 없는 경우 */
+        Optional<OnlineClass> optional = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .findFirst();
+
+        boolean present = optional.isPresent();
+        System.out.println("present = " + present);
+        
+        // 꺼낼때 값이 없으면 noSuchElementException 발생
+        /*
+        if (optional.isPresent()) { // null check
+            OnlineClass onlineClass = optional.get();
+            System.out.println("onlineClass.getTitle() = " + onlineClass.getTitle());
+        }
+        */
+
+        /* ifPresend() : if exist value then action */
+        optional.ifPresent(oc -> System.out.println("oc.getTitle() = " + oc.getTitle()));
+
+        /* orElse()  : if exist value then return value, if null action and return */
+        // 상수로 만들어져 있는 대상에 적합...
+        OnlineClass onlineClass = optional.orElse(createNewJpaClass());
+
+        /* orELseGet() : if exist value then get value, is null action */
+        // 동적으로 작업하는 경우 적합
+        OnlineClass onlineClass1 = optional.orElseGet(App::createNewJpaClass);
+        System.out.println("online = " + onlineClass1.getTitle());
+
+        /* orELseThrow() : 값있으면 가져오고 없으면 에러 리턴 */
+        // -> noSuchElementException 발생
+        OnlineClass onlineClass2 = optional.orElseThrow(IllegalStateException::new);
+        System.out.println("onlineClass2 = " + onlineClass2.getTitle());
+
+        /* filter() : 들어있는값 걸러내기 */
+        Optional<OnlineClass> onlineClass3 = optional.filter(oc -> !oc.isClosed());
+        System.out.println("onlineClass3.isEmpty() = " + onlineClass3.isEmpty());
+
+        /* map(function), flatMap(function) */
+        Optional<Integer> integer = optional.map(OnlineClass::getId);
+        System.out.println("integer = " + integer.isPresent());
+
+        Optional<Optional<Progress>> progress  = optional.map(OnlineClass::getProgress);
+        Optional<Progress> progress1 = progress.orElseThrow();
+        Progress progress2 = progress1.orElseThrow();
+
+        Optional<Progress> pro = optional.flatMap(OnlineClass::getProgress);
+
+        /*springClasses.stream()
+                .map()          // 1:1 맵핑
+             .flatMap()      // input 1, ouput 여러개..
+        */
+
+
+    }
+
+    private static OnlineClass createNewJpaClass() {
+        System.out.println("creating new online class");
+        return new OnlineClass(10, "jpa class", true);
     }
 }
--- src/main/java/com/study/java8/OnlineClass.java
+++ src/main/java/com/study/java8/OnlineClass.java
@@ -1,5 +1,7 @@
 package com.study.java8;
 
+import java.util.Optional;
+
 public class OnlineClass {
 
     private Integer id;
@@ -7,6 +9,8 @@ public class OnlineClass {
     private String title;
 
     private boolean closed;
+
+    public Progress progress;
 
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
@@ -37,4 +41,42 @@ public class OnlineClass {
     public void setClosed(boolean closed) {
         this.closed = closed;
     }
+
+
+    /*
+    public Progress getProgress() {
+        if (this.progress == null) {
+            // Runtime Exception -> 클라이언트 쪽 고통이 덜함
+            // Cheked Exception -> 에러처리를 강제함
+            // 에러가 발생하면 -> Call Stack -> StackTrace 출력 -> Resource 사용
+            // 로직을 처리할때 에러를 사용하는것은 좋은 습관이 아님
+
+            throw new IllegalStateException();
+        }
+        return progress;
+    }
+    */
+
+    public Optional<Progress> greProgress() {
+        // Optional.ofNullable(null 일수 있는 객체)
+        // return Optional.ofNullable(progress);   // 리턴타입으로 쓰는것만이 권장사항
+        return Optional.empty();
+    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
+    }
+
+    public <U> U getProgress() {
+    }
+
+
+    /*
+    public void setProgress(Optional<Progress> progress) {
+        if (progress.isPresent()) { // 이 메서드 자체가 NullPointException
+            progress.ifPresent(p -> this.progress = p);
+        }
+    }
+    */
+
 }
